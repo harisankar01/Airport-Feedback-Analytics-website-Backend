901c46dec8592f5f8750f4200536581a9ea42aa2
hari <hsankar@dinify.io>
2023-01-25 22:53:51 +0530

    Few changes regarding nltk storage

--- api/__init__.py
+++ api/__init__.py
@@ -1,6 +1,7 @@
-import nltk
-nltk.download("vader_lexicon", download_dir="/tmp")
-nltk.download("punkt", download_dir="/tmp")
-nltk.download("stopwords", download_dir="/tmp")
-nltk.download("averaged_perceptron_tagger", download_dir="/tmp")
-nltk.data.path.append("/tmp")
+# Need only when deploying for the first time
+# import nltk
+# nltk.download("vader_lexicon", download_dir="/tmp")
+# nltk.download("punkt", download_dir="/tmp")
+# nltk.download("stopwords", download_dir="/tmp")
+# nltk.download("averaged_perceptron_tagger", download_dir="/tmp")
+# nltk.data.path.append("/tmp")
